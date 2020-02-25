79494a6172f8e0cd2bb261ff264231b32c00587b
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-02-25 18:21:51 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+rihanna = Artist.create(name: "Rihanna")
+popsmoke = Artist.create(name: "Pop Smoke")
+lil = Artist.create(name: "Lil Wayne")
+elton = Artist.create(name: "Elton John")
+
+umbrella = Song.create(title: "Umbrella", artist_id: 1)
+famous = Song.create(title: "Famous", artist_id: 2)
+money = Song.create(title: "Money", artist_id: 3)
+ny = Song.create(title: "New York", artist_id: 4)
+love = Song.create(title: "Found Love", artist_id: 1)
+dreams = Song.create(title: "Dreams", artist_id: 3)
+stay = Song.create(title: "Stay", artist_id: 1)
