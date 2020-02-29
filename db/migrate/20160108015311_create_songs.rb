ea83423f5582745ba4cdd70df0fe153abbcf5b3d
github username <github email address>
2020-02-29 18:22:01 -0500

    Done.

--- db/migrate/20160108015311_create_songs.rb
+++ db/migrate/20160108015311_create_songs.rb
@@ -2,6 +2,8 @@ class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
       t.string :title
+      t.string :artist_name
+      t.integer :artist_id
 
       t.timestamps null: false
     end
